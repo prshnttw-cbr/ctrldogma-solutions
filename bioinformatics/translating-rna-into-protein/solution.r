37182adb12241a0f8395f8f194b300d3490dd333
Prashant Tiwari <prashantt@interns.cbr-iisc.ac.in>
2026-08-20 22:33:57 +0530

    Sync solution: Translating RNA into Protein (r)

---
+++ bioinformatics/translating-rna-into-protein/solution.r
@@ -0,0 +1,42 @@
+rna <- trimws(readLines(file("stdin"), n = 1))  # The RNA sequence to translate
+
+solve <- function(rna) {
+  codon_table <- c(
+    UUU="F", UUC="F", UUA="L", UUG="L",
+    UCU="S", UCC="S", UCA="S", UCG="S",
+    UAU="Y", UAC="Y",
+    UGU="C", UGC="C", UGG="W",
+    
+    CUU="L", CUC="L", CUA="L", CUG="L",
+    CCU="P", CCC="P", CCA="P", CCG="P",
+    CAU="H", CAC="H", CAA="Q", CAG="Q",
+    CGU="R", CGC="R", CGA="R", CGG="R",
+    
+    AUU="I", AUC="I", AUA="I", AUG="M",
+    ACU="T", ACC="T", ACA="T", ACG="T",
+    AAU="N", AAC="N", AAA="K", AAG="K",
+    AGU="S", AGC="S", AGA="R", AGG="R",
+    
+    GUU="V", GUC="V", GUA="V", GUG="V",
+    GCU="A", GCC="A", GCA="A", GCG="A",
+    GAU="D", GAC="D", GAA="E", GAG="E",
+    GGU="G", GGC="G", GGA="G", GGG="G"
+  )
+  
+  stop_codons <- c("UAA", "UAG", "UGA")
+  protein <- character()
+  
+  for (i in seq(1, nchar(rna), by = 3)) {
+    codon <- substr(rna, i, i + 2)
+    
+    if (codon %in% stop_codons) {
+      break
+    }
+    
+    protein <- c(protein, codon_table[codon])
+  }
+  
+  cat(paste0(protein, collapse = ""))
+}
+
+solve(rna)
